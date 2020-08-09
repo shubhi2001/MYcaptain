49440667f0038b7fd1075277ec5f678f380f0bb5
STUTI SRIVASTAVA <69310359+shubhi2001@users.noreply.github.com>
2020-08-09 23:53:44 +0530

    Add files via upload

---
+++ vote_ eligibility.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout<<"enter your age :"<<age;
+	if(age<=18)
+	 {
+	 cout<<"you are eligible for voting";
+	 
+	 
+     }
+	else 
+	 cout<<"you are not eligible for voting";
+	  
+}
