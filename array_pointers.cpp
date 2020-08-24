c7eb7ff8d6c95fe8cd8eae63053c4e9e8ddeaf4d
STUTI SRIVASTAVA <69310359+shubhi2001@users.noreply.github.com>
2020-08-24 11:36:49 +0530

    Add files via upload

---
+++ array_pointers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+	int arr[5]={1,2,3,4,5};
+	for(int i=0;i<5;i++)
+	{
+	   cout << "the "<<i<<"th element of the array is :"<<endl;
+	   cout << *(arr + i)<<endl ;	
+	}
+}
