b94d149ca4013621cbe02ce0e94b3afbd2842568
STUTI SRIVASTAVA <69310359+shubhi2001@users.noreply.github.com>
2020-09-11 19:10:51 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    int b=10;
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    printf("value of a is %d\n",a);
+    printf("value of b is %d\n",b);
+    return 0;
+}
+
