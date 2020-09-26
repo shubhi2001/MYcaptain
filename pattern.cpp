4ac45ff9644e0e27cd3b7acd723a7daca0c1780a
STUTI SRIVASTAVA <69310359+shubhi2001@users.noreply.github.com>
2020-09-26 14:35:44 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+  for(int i=1;i<=6;i++)
+
+  {
+
+    for(int j=1;j<=i;j++)
+
+    {
+
+      cout << "01";
+
+    }
+
+    cout <<endl;
+
+  }
+
+return 0;
+
+}
