25c8d5c54d7fbb48f8ceb37416798d528ba60b79
STUTI SRIVASTAVA <69310359+shubhi2001@users.noreply.github.com>
2020-09-26 20:59:15 +0530

    Add files via upload

---
+++ count_dig.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int count_dig(int num)
+{
+    if(num == 0)
+        return 0;
+    return (num%10) + count_dig(num/10);
+}
+int main()
+{
+    int num;
+    printf("enter a number");
+    scanf("%d",num);
+   int sum = count_dig(num);
+  printf("sum is %d",sum);
+  return 0;
+}
+
